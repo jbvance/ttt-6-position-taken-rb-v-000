a020498d8a01689264c94eb704e0e968d4097484
Jason Vance <jbvance@gmail.com>
2017-11-14 18:53:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,2 @@
 # code your #position_taken? method here!
-def position_taken?(board, index)
-  if board[index] == " " || board[index].strip == "" || board[index] == nil
-    return false
-  end
-  return true
-end
 
-board = []
-index = 0
-puts position_index(board, index)
